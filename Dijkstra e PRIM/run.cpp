e885d3bb29a11f50e257e6711031e11a686a8fcf
Kened Oliveira <kenedwanderson@outlook.com>
2017-10-13 19:00:29 -0300

    agora vai

---
+++ Dijkstra e PRIM/run.cpp
@@ -0,0 +1,14 @@
+#include "mdl.h"
+
+int main()
+{
+	//---------PRIM--------
+	std::cout<<"Prim" <<std::endl;
+	primMST();
+
+	//---------Dijkstra--------
+	//std::cout<<" Dijkstra "<<std::endl;
+	//dijkstra(0);
+	
+	return 0;
+}
---
+++ Insertion e Selection Sorts/Insertion Sort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, atual;
+            int [] vetor = new int[] { 9,5,2,1,41,0,1952,22,3,852,159,33,6};
+
+
+            for (i = 1; i < vetor.Length; i++)
+            {
+                atual = vetor[i];
+                j = i;
+                while ((j > 0) && (vetor[j - 1] > atual))
+                {
+                    vetor[j] = vetor[j - 1];
+                    j = j - 1;
+                }
+                vetor[j] = atual;
+            }
+            Console.WriteLine(String.Join(", ", vetor));
+            Console.ReadLine();
+        }
+    }
+}
