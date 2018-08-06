e74640d4210841fd132fc0825d5d4e5cabd81352
Kened Wanderson <kenedwanderson@live.com>
2018-08-05 22:24:49 -0300

    Tempo de execução

--- Insertion e Selection Sorts/insertion.py
+++ Insertion e Selection Sorts/insertion.py
@@ -5,16 +5,19 @@ Algoritimo de ordenação insertion sort produzido para a disciplina de APA
 '''
 
 import sys
+import timeit
+
 if len(sys.argv) != 2:
     arq = open("123.txt","r")
 else:
-    arq = open(sys.argv[1],"r")
+    arq = open("../instancias-numericas/"+sys.argv[1],"r")
 lista = []
 for linha in arq:
     lista.append(int(linha))
 
 
-print("\n-------------------------Desordenada ------------------------------\n ",lista)
+# print("\n-------------------------Desordenada ------------------------------\n ",lista)
+Ti = timeit.default_timer()                         #medindo tempo inicial
 
 for i in range(1,len(lista)):
     escolha = lista[i]                     #pega o segundo elemento da lista
@@ -24,4 +27,6 @@ for i in range(1,len(lista)):
         k -= 1                             #e decrementa o iterado
     lista[k] = escolha                     #insere o escolhido na posição de ordenação
 
-print("\n--------------------------Ordenada ----------------------------------\n ",lista)
+Tf = timeit.default_timer()                         #medindo tempo final
+print("Tempo total: {:f}s do InsertionSort".format(Tf-Ti))
+# print("\n--------------------------Ordenada ----------------------------------\n ",lista)
--- Insertion e Selection Sorts/selection.py
+++ Insertion e Selection Sorts/selection.py
@@ -5,16 +5,20 @@ Algoritimo de ordenação selection sort produzido para a disciplina de APA
 '''
 
 import sys
+import timeit
+
 if len(sys.argv) != 2:
     arq = open("123.txt","r")
 else:
-    arq = open(sys.argv[1],"r")
+    arq = open("../instancias-numericas/"+sys.argv[1],"r")
 lista = []
 for linha in arq:
     lista.append(int(linha))
 
 
-print("\n-------------------------Desordenada ------------------------------\n ",lista)
+# print("\n-------------------------Desordenada ------------------------------\n ",lista)
+
+Ti = timeit.default_timer()                         #medindo tempo inicial
 
 for i in range(len(lista)):                         #percorre toda a lista
     menor = i                                       #pega o menor indice
@@ -23,4 +27,6 @@ for i in range(len(lista)):                         #percorre toda a lista
             menor = k                               #guarda a posição do menor
     lista[i], lista[menor] = lista[menor], lista[i] #faz um swap(troca) do menor item para o primeiro elemento ainda não ordenado
 
-print("\n--------------------------Ordenada ----------------------------------\n ",lista)
+Tf = timeit.default_timer()                         #medindo tempo final
+print("Tempo total: {:f}s do SlectionSort".format(Tf-Ti))
+# print("\n--------------------------Ordenada ----------------------------------\n ",lista)
