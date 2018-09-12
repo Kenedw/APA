a27a6bb90677b52389e94b3bddce2e4f0e4b002d
Kened Wanderson <kenedwanderson@live.com>
2018-09-11 22:34:30 -0300

    Remoção dos logs Inversão do iterador

--- CountingSort e RadixSort/RadixSort.py
+++ CountingSort e RadixSort/RadixSort.py
@@ -12,38 +12,45 @@ if len(sys.argv) != 2:
 else:
     arq = open("../instancias-numericas/"+sys.argv[1],"r")
 lista = []
+
 for linha in arq:
     lista.append(int(linha))
 
-def insertion(lista):
-    for i in range(1,len(lista)):
-        escolha = lista[i]                     #pega o segundo elemento da lista
-        k = i                                  #pega o tamanho do interador a ser percorrido
-        while k > 0 and escolha < lista[k-1]:  #vefica se o escolhido he menor que o numero anterior da lista
-            lista[k] = lista[k - 1]            #se sim, move os anteriores ate ele ser meior ou ser o primeiro
-            k -= 1                             #e decrementa o iterado
-        lista[k] = escolha                     #insere o escolhido na posicao de ordenacao
-    return lista
+# print("\n-------------------------Desordenada ------------------------------\n ",lista)
+def CountingSort(data,digit):
+    aux_count = [0 for i in range(10)]              #Inicializa o auxiliar com 0
+    #------------------------VERIFICA RECORRENCIA-----------------------------
+    for i in data:                                  #Verifica ocorrencia
+        least = list(map(int,str(i)))
+        if(digit > len(least)-1):                     #pega zero se nao ouver outro mais significativo
+            least = 0
+        else:
+            least = least[digit-1]
+        aux_count[least]+=1
+    #------------------------ORDENA OS INDICES-----------------------------
+    for i in range(1,len(aux_count)):               #Ordena o indices da auxiliar
+        aux_count[i] += aux_count[i-1]
 
-print("\n-------------------------Desordenada ------------------------------\n ",lista)
-def RadixSort(data):
-    lens = len(data)
-    bins = [[] for i in range(10)]
-    result = [[] for i in range(lens)]
-    for i in data:
-        least = map(int,str(i))
-        least = least[0]
-        bins[least].append(i)
-    count = 0
-    for i in bins:
-        for j in i:
-            result[count] = j
-            count +=1
-            insertion(result)
-    return result
+    #-------------POPULA A SAIDA DEACORDO COM OS INDICES---------------------
+    output = [0 for i in range(len(data))]          #inicio o vetor de saia com o tamanho de data
+    for i in reversed(data):                                  #passa os valores ordenados para a saida
+        least = list(map(int,str(i)))
+        if(digit > len(least)-1):                     #pega zero se nao ouver outro mais significativo
+            least = 0
+        else:
+            least = least[digit-1]
+        output[aux_count[least]-1] = i
+        aux_count[least]-=1
+    return output
 
-Ti = timeit.default_timer()                         #medindo tempo inicial
-lista = RadixSort(lista)                            #chamada do CountingSort passando o arquivo que foi aberto
-Tf = timeit.default_timer()                         #medindo tempo final
-print("Tempo total: {:f}s do MergeSort".format(Tf-Ti))
-print("\n--------------------------Ordenada ----------------------------------\n ",lista)
+def radixSort(data,C):
+    for digit in range(C):
+        data = CountingSort(data,digit)
+    return data
+
+
+Ti = timeit.default_timer()                                             #medindo tempo inicial
+lista = radixSort(lista,len(list(map(int,str(max(lista))))))            #chamada do CountingSort passando o arquivo que foi aberto
+Tf = timeit.default_timer()                                             #medindo tempo final
+print("Tempo total: {:f}s do RadixSort".format(Tf-Ti))
+# print("\n--------------------------Ordenada ----------------------------------\n ",lista)
