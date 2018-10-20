d1061bd23bb8b9133d74a15890ee570f37c3e074
Kened Wanderson <kenedwanderson@live.com>
2018-10-20 14:19:18 -0300

    fix para importação

--- Coloração de Vértices/heuristica.py
+++ Coloração de Vértices/heuristica.py
@@ -7,8 +7,15 @@ Em 1976, Welsh e Powell propuseram o algoritmo LF (Large First), que analisa os 
 import sys
 import numpy as np
 
-def main(arg):
-    V_E = InputFile(arg)
+
+#se AdjacentList iqual a -1, lista de adjacencias seram buscadas no FilePath
+def heuristica_gulosa(FilePath,AdjacentList):
+
+    if(AdjacentList != -1):
+        V_E = AdjacentList
+    else:
+        V_E = CreateAdjList(FilePath)
+
     color = np.zeros(max(V_E)[0]+1)
     for i in V_E:
         color[i[0]] = 1
@@ -16,30 +23,30 @@ def main(arg):
             if(color[i[0]] == color[j]):
                 color[i[0]] += 1
                 # print("colorindo {} ligado a {} com {}".format(i[0],j,color[i[0]]))
-    print(color)
-    print("Numero de cores {}".format(int(max(color))))
+    # print(color)
+    # print("Numero de cores {}".format(int(max(color))))
+    return color
 
-def InputFile(arg): #monta a lista de
+#Cria uma lista de adjacencias, entrar com o diretorio do arquivo
+def CreateAdjList(arg): #monta a lista de adjacencia
     out = []
-    after = -1
+    before = -1
     index = -1
-
-    if len(arg) != 2:
-        arq = open("instancias/DSJC125.1.col","r")
+    if (len(arg) != 2):
+        arq = open("instancias/DSJC250.9.col","r")
     else:
         arq = open("instancias/" + arg[1],"r")
     for j in arq:
         if(j[0] == 'e'):
             aux = map(int,j.split()[1:])
-            if(aux[0] != after):
+            if(aux[0] != before):
                 out.append(aux)
-                after = aux[0]
+                before = aux[0]
                 index +=1
             else:
                 out[index].append(aux[1])
     arq.close()
     return out
 
-
 if __name__ == "__main__":
-    main(sys.argv)
+    heuristica_gulosa(sys.argv,-1)
